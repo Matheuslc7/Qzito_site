50d4025da30b1437a9ae8c9f86cae56bbc51e268
matheuslc7 <matheus.lcandido7@gmail.com>
2024-01-17 15:35:41 -0300

    3.0.5

--- script.js
+++ script.js
@@ -5,118 +5,119 @@ document.addEventListener('DOMContentLoaded', () => {
     const subtotalList = document.getElementById('subtotalList');
     const totalElement = document.getElementById('total');
     const barcodes = JSON.parse(localStorage.getItem('barcodes')) || {};
-
+  
     // Função para atualizar a exibição dos códigos de barras na tela
     const updateBarcodeDisplay = () => {
-        barcodeList.innerHTML = '';
-        let count = 0;
-        for (const barcode in barcodes) {
-            const barcodeDiv = document.createElement('div');
-            barcodeDiv.classList.add('barcode');
-            barcodeDiv.textContent = `Código : ${++count}: ${barcode}`;
-            barcodeList.appendChild(barcodeDiv);
-        }
+      barcodeList.innerHTML = '';
+      let count = 0;
+      for (const barcode in barcodes) {
+        const barcodeDiv = document.createElement('div');
+        barcodeDiv.classList.add('barcode');
+        barcodeDiv.textContent = `Código : ${++count}: ${barcode}`;
+        barcodeList.appendChild(barcodeDiv);
+      }
     };
-
+  
     // Função para atualizar a exibição dos subtotais na tela
     const updateSubtotalDisplay = () => {
-        subtotalList.innerHTML = '<h2>Subtotal por Código de Barras:</h2>';
-        for (const barcode in barcodes) {
-            const subtotalDiv = document.createElement('div');
-            subtotalDiv.classList.add('subtotal');
-            subtotalDiv.textContent = `Código ${barcode}: ${barcodes[barcode]}`;
-            subtotalList.appendChild(subtotalDiv);
-        }
+      subtotalList.innerHTML = '<h2>Subtotal por Código de Barras:</h2>';
+      for (const barcode in barcodes) {
+        const subtotalDiv = document.createElement('div');
+        subtotalDiv.classList.add('subtotal');
+        subtotalDiv.textContent = `Código ${barcode}: ${barcodes[barcode]}`;
+        subtotalList.appendChild(subtotalDiv);
+      }
     };
-
+  
     // Carregar códigos de barras salvos no localStorage ao carregar a página
     updateBarcodeDisplay();
     updateSubtotalDisplay();
-
+  
     barcodeInput.addEventListener('input', (event) => {
-        const barcodeValue = event.target.value.trim();
-
-        if (barcodeValue.length === 13) {
-            if (!barcodes[barcodeValue]) {
-                barcodes[barcodeValue] = 0;
-            }
-            barcodes[barcodeValue] += 1;
-
-            // Atualizar a exibição dos códigos de barras
-            updateBarcodeDisplay();
-
-            // Limpar o campo de entrada
-            barcodeInput.value = '';
-
-            // Atualizar a exibição dos subtotais
-            updateSubtotalDisplay();
-
-            // Calcular e exibir o total
-            const total = Object.values(barcodes).reduce((acc, curr) => acc + curr, 0);
-            totalElement.textContent = `Total de códigos lidos: ${total}`;
-
-            // Salvar os códigos de barras no localStorage
-            localStorage.setItem('barcodes', JSON.stringify(barcodes));
+      const barcodeValue = event.target.value.trim();
+  
+      if (barcodeValue.length === 13) {
+        if (!barcodes[barcodeValue]) {
+          barcodes[barcodeValue] = 0;
         }
+        barcodes[barcodeValue] += 1;
+  
+        // Atualizar a exibição dos códigos de barras
+        updateBarcodeDisplay();
+  
+        // Limpar o campo de entrada
+        barcodeInput.value = '';
+  
+        // Atualizar a exibição dos subtotais
+        updateSubtotalDisplay();
+  
+        // Calcular e exibir o total
+        const total = Object.values(barcodes).reduce((acc, curr) => acc + curr, 0);
+        totalElement.textContent = `Total de códigos lidos: ${total}`;
+  
+        // Salvar os códigos de barras no localStorage
+        localStorage.setItem('barcodes', JSON.stringify(barcodes));
+      }
     });
-
+  
     // Lidar com o clique do botão de envio no lugar do evento de envio do formulário
-    myForm.addEventListener('touchstart', async (event) => {
-        // Verificar se o clique ocorreu no botão de envio
-        if (event.target.type === 'submit') {
-            event.preventDefault();
-
-            // Extrair os dados do input e das divs
-            const refValue = document.getElementById('ref').value.trim();
-            const subtotalData = Array.from(subtotalList.children).map(div => div.textContent);
-            const totalData = totalElement.textContent;
-
-            // Obter a data e hora atual em horário de Brasília
-            const dateTimeBrasilia = luxon.DateTime.now().setZone('America/Sao_Paulo').toISO();
-
-            try {
-                // Criar um objeto com os dados que você deseja enviar
-                const postData = {
-                    ref: refValue,
-                    subtotalList: subtotalData,
-                    total: totalData,
-                    datetime: dateTimeBrasilia  // Adiciona a data e hora atual em Brasília
-                };
-
-                // Enviar os dados para o servidor usando a função fetch
-                const response = await fetch('https://api.sheetmonkey.io/form/tU48xyPpPN3DsVVefCT7d6', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(postData)
-                });
-
-                // Verificar se a solicitação foi bem-sucedida
-                if (response.ok) {
-                    // Limpar os valores dentro das divs após o envio bem-sucedido
-                    barcodeList.innerHTML = '';
-                    subtotalList.innerHTML = '';
-                    totalElement.textContent = '';
-
-                    // Limpar os dados específicos no localStorage
-                    localStorage.removeItem('barcodes');
-
-                    // Limpar o valor do input "ref"
-                    document.getElementById('ref').value = '';
-
-                    // Exibir mensagem na tela
-                    if (window.confirm('Remessa enviada com sucesso. Clique em OK para continuar.')) {
-                        // Redirecionar para a página inicial (index.html) após o OK ser clicado
-                        window.location.href = 'index.html';
-                    }
-                } else {
-                    // Caso a solicitação falhe, você pode lidar com isso de acordo com suas necessidades
-                    console.error('Erro ao enviar os dados para o servidor.');
-                }
-            } catch (error) {
-                console.error('Erro na solicitação:', error);
-            }
+    myForm.addEventListener('click', async (event) => {
+      // Verificar se o clique ocorreu no botão de envio
+      if (event.target.type === 'submit') {
+        event.preventDefault();
+  
+        // Extrair os dados do input e das divs
+        const refValue = document.getElementById('ref').value.trim();
+        const subtotalData = Array.from(subtotalList.children).map(div => div.textContent);
+        const totalData = totalElement.textContent;
+  
+        // Obter a data e hora atual em horário de Brasília
+        const dateTimeBrasilia = luxon.DateTime.now().setZone('America/Sao_Paulo').toISO();
+  
+        try {
+          // Criar um objeto com os dados que você deseja enviar
+          const postData = {
+            ref: refValue,
+            subtotalList: subtotalData,
+            total: totalData,
+            datetime: dateTimeBrasilia  // Adiciona a data e hora atual em Brasília
+          };
+  
+          // Enviar os dados para o servidor usando a função fetch
+          const response = await fetch('https://api.sheetmonkey.io/form/tU48xyPpPN3DsVVefCT7d6', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+          });
+  
+          // Verificar se a solicitação foi bem-sucedida
+          if (response.ok) {
+            // Limpar os valores dentro das divs após o envio bem-sucedido
+            barcodeList.innerHTML = '';
+            subtotalList.innerHTML = '';
+            totalElement.textContent = '';
+  
+            // Limpar os dados específicos no localStorage
+            localStorage.removeItem('barcodes');
+  
+            // Limpar o valor do input "ref"
+            document.getElementById('ref').value = '';
+  
+            // Exibir mensagem na tela
+            alert('Remessa enviada com sucesso');
+  
+            // Redirecionar para a página inicial (index.html)
+            window.location.href = 'index.html';
+          } else {
+            // Caso a solicitação falhe, você pode lidar com isso de acordo com suas necessidades
+            console.error('Erro ao enviar os dados para o servidor.');
+          }
+        } catch (error) {
+          console.error('Erro na solicitação:', error);
         }
+      }
     });
-});
+  });
+  
